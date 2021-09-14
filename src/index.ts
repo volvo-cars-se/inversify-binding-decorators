d0c5d1506f0914711e42fd2c86f1b1f9beb3253b
Max Bolotin <maks.bolotin@gmail.com>
2021-09-14 14:09:03 +0200

    (bugfix) type issues on default export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import getProvideDecorator from "./decorator/provide";
 import getFluentProvideDecorator from "./decorator/fluent_provide";
 import getProviderModuleBuilder from "./factory/module_factory";
+import { interfaces as inversifyInterfaces } from "inversify";
+import interfaces from "./interfaces/interfaces";
 
-function createProvideContext() {
+interface ProvideContext {
+    provide: (serviceIdentifier: inversifyInterfaces.ServiceIdentifier<any>, force?: boolean) => any;
+    fluentProvide: (serviceIdentifier: inversifyInterfaces.ServiceIdentifier<any>) => interfaces.ProvideInWhenOnSyntax<any>;
+    buildProviderModule: () => inversifyInterfaces.ContainerModule;
+}
+
+function createProvideContext(): ProvideContext {
     const metadataKey = Symbol("metadata_key");
 
     const provide = getProvideDecorator(metadataKey);
